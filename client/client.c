64f54259d98337f4111f2dd7b63a7559d24ce5c3
jtronea <chenbobazi@163.com>
2016-04-30 19:55:07 +0800

    模拟发送http请求

--- client/client.c
+++ client/client.c
@@ -1,5 +1,5 @@
 #include "client.h"
-#define WORKERTHREADS 50
+#define WORKERTHREADS 1
 pthread_t threadspool[WORKERTHREADS];       //存放线程
 
 void* handle_task(void* data)
@@ -9,18 +9,27 @@ void* handle_task(void* data)
     sfd = socket(AF_INET, SOCK_STREAM, 0);
     bzero(&serveraddr, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
+    //serveraddr.sin_port = htons(3333);
+    //inet_pton(AF_INET, "127.0.0.1", &serveraddr.sin_addr);
     serveraddr.sin_port = htons(3333);
     inet_pton(AF_INET, "127.0.0.1", &serveraddr.sin_addr);
     int err = connect(sfd, (SA*)&serveraddr, sizeof(serveraddr));
     printf("connect err %d\n",err);
-    char buf[8] = {"12345"};
+    char buf[512] = {"GET /?test=troneacheng HTTP/1.1\r\nUser-Agent: curl/7.19.7 (x86_64-redhat-linux-gnu) libcurl/7.19.7 NSS/3.13.1.0 zlib/1.2.8 libidn/1.18 libssh2/1.2.2\r\nHost: 127.0.0.1:3333\r\nAccept: */*\r\n\r\n"};
     err = write(sfd, buf, sizeof(buf));
     printf("finish send data on fd %d\n",sfd);
-    char output[8] = {0};
+    char output[512] = {0};
     err = read(sfd, output, sizeof(output));
-    printf("finish read data on fd %d\n",sfd);
-    output[7] = '\n';
-    write(1,  output, sizeof(output));
+    if(err >= 0)
+    {
+        printf("finish read data on fd %d\n",sfd);
+        output[err] = '\n';
+        write(1,  output, sizeof(output));
+    }
+    else
+    {
+        printf("read data on fd %d error\n",sfd);
+    }
 }
 
 int spawn_multi_workerthread(){
